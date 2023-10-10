1b498c5aacd4abbc1ba232b792e34cf43fedffb2
kem02 <113561273+kem02@users.noreply.github.com>
2023-10-10 04:38:43 +0000

    #47 test our terra home on terratowns test town

--- variables.tf
+++ variables.tf
@@ -10,9 +10,9 @@ variable "terratowns_access_token" {
   type = string
 }
 
-variable "bucket_name" {
-  type = string
-}
+# variable "bucket_name" {
+#   type = string
+# }
 
 variable "index_html_filepath" {
   type = string
